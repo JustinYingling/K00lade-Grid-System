1677f42b434c9bdb247bc5b7acf80310bcf8f9ac
Justin Y <jay6198@gmail.com>
2014-10-13 10:20:28 -0700

    Added images folder to /css and index.php file

---
+++ index.php
@@ -0,0 +1,85 @@
+<?php include ('inc/_header.html'); ?>
+<!-- head -->
+
+ <!-- slider -->
+ <div class="row slider">
+   
+  <div class="col-1">
+    <div class="col">Slider</div>
+  </div> 
+        
+ </div>
+ <!-- ./slider-->
+ 
+  <!-- seach bar -->
+ <div class="row tagline">
+   
+  <div class="col-1">
+    <div class="col">Search Bar</div>
+  </div> 
+        
+ </div>
+ <!-- ./search bar-->
+ 
+ 
+ <!--page 75/25 -->
+ <div class="row l75">
+   
+  <div class="col-75">
+     <div class="col">75</div>
+  </div>
+        
+  <div class="col-25">
+     <div class="col">25</div>
+  </div>
+
+ </div>
+ <!-- ./page 75/25 -->  
+ 
+  
+ <!--page 25/75 -->   
+ <div class="row r75">
+   
+  <div class="col-25">
+    <div class="col">25</div>
+  </div>
+        
+  <div class="col-75">
+    <div class="col">75</div>
+  </div>
+        
+ </div>
+ <!-- ./page 25/75 -->    
+ 
+ 
+ <!-- tagline -->
+ <div class="row tagline">
+   
+  <div class="col-1">
+    <div class="col">Tagline</div>
+  </div> 
+        
+ </div>
+ <!-- ./tagline-->
+ 
+   
+ <!--page 3-col -->   
+ <div class="row">
+   
+  <div class="col-3">
+    <div class="col">3</div>
+  </div>
+        
+  <div class="col-3">
+    <div class="col">3</div>
+  </div>
+  
+  <div class="col-3">
+    <div class="col">3</div>
+  </div>
+        
+ </div>
+ <!-- ./page 3-col -->  
+ 
+<!-- footer -->
+<?php include('inc/_footer.html'); ?>
